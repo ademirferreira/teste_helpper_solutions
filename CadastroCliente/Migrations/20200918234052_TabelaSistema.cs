86f719a214ecd2bd8a651599510818c016b28842
Ademir Ferreira <whoisademir@gmail.com>
2020-09-18 20:44:17 -0300

    Create migrations and scaffold Cliente

---
+++ CadastroCliente/Migrations/20200918234052_TabelaSistema.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CadastroCliente.Migrations
+{
+    public partial class TabelaSistema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Nome = table.Column<string>(maxLength: 100, nullable: false),
+                    Documento = table.Column<string>(maxLength: 14, nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Telefone = table.Column<string>(maxLength: 11, nullable: false),
+                    TipoCliente = table.Column<int>(nullable: false),
+                    Cep = table.Column<string>(maxLength: 8, nullable: false),
+                    Logradouro = table.Column<string>(maxLength: 200, nullable: false),
+                    Numero = table.Column<string>(maxLength: 50, nullable: false),
+                    Complemento = table.Column<string>(nullable: true),
+                    Bairro = table.Column<string>(maxLength: 100, nullable: false),
+                    Cidade = table.Column<string>(maxLength: 100, nullable: false),
+                    Estado = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
